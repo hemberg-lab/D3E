9cf0d4e33eb689fbb244478fb9aa305040030b6f
mdelmans <md656@cam.ac.uk>
2015-06-03 14:00:03 +0100

    Adding license

--- D3ECmd.py
+++ D3ECmd.py
@@ -11,6 +11,23 @@ scipy 0.13.0b1
 numpy 1.8.0rc1
 sympy.mpmath 0.18
 
+Copyright 2015 Mihails Delmans, Martin Hemberg
+
+This file is part of D3E.
+
+D3E is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+D3E is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with D3E.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 from __future__ import division
 
